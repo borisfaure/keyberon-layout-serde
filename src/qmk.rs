32c93a7061e15a8b02571bec7746b6c1d4ebed2c
Boris Faure <boris@fau.re>
2023-03-18 14:04:31 +0100

    src: deserialize some qmk keycodes

---
+++ src/qmk.rs
@@ -0,0 +1,60 @@
+use serde::Deserialize;
+use serde_json;
+use serde_json::Error as JsonError;
+
+#[derive(Deserialize, Debug)]
+pub enum QmkKeyCode {
+    #[serde(rename = "KC_CAPS")]
+    KcCaps,
+    #[serde(rename = "KC_PGUP")]
+    KcPgUp,
+    #[serde(rename = "KC_DEL")]
+    KcDel,
+    #[serde(rename = "KC_END")]
+    KcEnd,
+    #[serde(rename = "KC_PGDN")]
+    KcPgDn,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct QmkKeymap {
+    pub version: u32,
+    pub notes: String,
+    pub documentation: String,
+    pub keyboard: String,
+    pub keymap: String,
+    pub layout: String,
+    pub layers: Vec<Vec<QmkKeyCode>>,
+    pub author: String,
+}
+
+#[cfg(test)]
+mod qmk_tests {
+    use crate::qmk::JsonError;
+    use crate::qmk::QmkKeymap;
+    #[test]
+    fn test_simple_one_layer() {
+        let json = r#"
+{
+  "version": 1,
+  "notes": "",
+  "documentation": "",
+  "keyboard": "pad",
+  "keymap": "map",
+  "layout": "LAYOUT_ortho_1x5",
+  "layers": [
+    [
+      "KC_CAPS",
+      "KC_PGUP",
+      "KC_DEL",
+      "KC_END",
+      "KC_PGDN"
+    ]
+  ],
+  "author": ""
+}"#;
+        let qmk_res: Result<QmkKeymap, JsonError> = serde_json::from_str(&json);
+        println!("{:?}", qmk_res);
+        assert!(qmk_res.is_ok());
+    }
+}
