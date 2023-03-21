5e4ae7dc1d7326d6322069e6e0d0bd09e293affb
Boris Faure <boris@fau.re>
2023-03-21 23:48:10 +0100

    qmk: try to deserialize QmkAction

--- src/qmk.rs
+++ src/qmk.rs
@@ -1,8 +1,126 @@
 use crate::qmk_keycodes::QmkKeyCode;
+use serde::de::{self, Deserializer, IntoDeserializer};
 use serde::Deserialize;
 use serde_json;
 use serde_json::Error as JsonError;
 
+#[derive(Debug)]
+pub enum QmkAction {
+    /// A key code
+    KeyCode(QmkKeyCode),
+    /// MO()
+    MomentaryTurnLayerOn(u8),
+    /// TG()
+    ToggleLayer(u8),
+}
+
+/// Lexer item
+#[derive(Debug, Clone)]
+enum LexItem<'a> {
+    /// A token
+    Token(&'a str),
+    /// A parenthesis opening or closing
+    Parenthesis(char),
+    /// A number
+    Number(usize),
+}
+
+/// Transform @input into a vector of tokens
+fn lex<'a>(input: &'a str) -> Result<Vec<LexItem<'a>>, String> {
+    let mut result = Vec::new();
+
+    let chars = input.chars();
+    let mut it = chars.enumerate();
+    let mut pos = it.next();
+    while let Some((i, c)) = pos {
+        match c {
+            '0'..='9' => {
+                let mut num: usize = 0;
+                while let Some((i, c)) = pos {
+                    match c {
+                        '0'..='9' => {
+                            num = (c as usize - '0' as usize) + num * 10;
+                            pos = it.next();
+                        }
+                        _ => break,
+                    }
+                }
+                result.push(LexItem::Number(num));
+            }
+            'A'..='Z' | '_' => {
+                let start = i;
+                let mut end = i;
+                pos = it.next();
+                while let Some((i, c)) = pos {
+                    match c {
+                        'A'..='Z' | '_' => {
+                            end = i;
+                            pos = it.next();
+                        }
+                        _ => break,
+                    }
+                }
+                result.push(LexItem::Token(&input[start..=end]));
+            }
+            '(' | ')' => {
+                result.push(LexItem::Parenthesis(c));
+            }
+            ' ' | ',' => {}
+            _ => {
+                return Err(format!("caracter '{}'@{} while parsing {}", c, i, input));
+            }
+        }
+    }
+    Ok(result)
+}
+
+/// Parse a vector of tokens
+fn parse(tokens: &[LexItem]) -> Result<QmkAction, String> {
+    if tokens.is_empty() {
+        return Err(String::from("no tokens"));
+    }
+    if tokens.len() == 1 {
+        if let LexItem::Token(tok) = tokens[0] {
+            let d: de::value::StrDeserializer<de::value::Error> = tok.into_deserializer();
+            if let Ok(kc) = QmkKeyCode::deserialize(d) {
+                return Ok(QmkAction::KeyCode(kc));
+            } else {
+                return Err(format!("invalid keycode '{}'", tok));
+            }
+        } else {
+            return Err(String::from("Expecting token"));
+        }
+    }
+    if let LexItem::Token(tok) = tokens[0] {
+        match tok {
+            "MO" => return Err(format!(" got {}", tok)),
+            _ => return Err(format!("unsupported command {}", tok)),
+        }
+    } else {
+        return Err(String::from("Expecting token"));
+    }
+}
+
+impl<'de> Deserialize<'de> for QmkAction {
+    fn deserialize<D>(deserializer: D) -> Result<QmkAction, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        let tokens = lex(&s);
+        if let Err(s) = tokens {
+            return Err(de::Error::custom(s));
+        }
+        let tokens = tokens.unwrap();
+        match parse(tokens.as_slice()) {
+            Ok(qa) => Ok(qa),
+            Err(s) => Err(de::Error::custom(s)),
+        }
+    }
+}
+
+/// The basic structure for the layout JSON files used in
+/// [qmk configurator](https://config.qmk.fm/)
 #[derive(Deserialize, Debug)]
 pub struct QmkKeymap {
     pub version: u32,
@@ -11,7 +129,7 @@ pub struct QmkKeymap {
     pub keyboard: String,
     pub keymap: String,
     pub layout: String,
-    pub layers: Vec<Vec<QmkKeyCode>>,
+    pub layers: Vec<Vec<QmkAction>>,
     pub author: String,
 }
 
@@ -20,7 +138,7 @@ mod qmk_tests {
     use crate::qmk::JsonError;
     use crate::qmk::QmkKeymap;
     #[test]
-    fn test_simple_one_layer() {
+    fn test_basic_one_layer_only_keycodes() {
         let json = r#"
 {
   "version": 1,
@@ -36,6 +154,34 @@ mod qmk_tests {
       "KC_DEL",
       "KC_END",
       "KC_PGDN"
+    ]
+  ],
+  "author": ""
+}"#;
+        let qmk_res: Result<QmkKeymap, JsonError> = serde_json::from_str(&json);
+        assert!(qmk_res.is_ok());
+    }
+
+    #[test]
+    fn test_layers() {
+        let json = r#"
+{
+  "version": 1,
+  "notes": "",
+  "documentation": "",
+  "keyboard": "pad",
+  "keymap": "map",
+  "layout": "LAYOUT_ortho_1x3",
+  "layers": [
+    [
+      "KC_SPACE",
+      "MO(1)",
+      "TG(1)"
+    ],
+    [
+      "KC_ENTER",
+      "MO(1)",
+      "TG(0)"
     ]
   ],
   "author": ""
