15f75bca90afa7a03f59ff453c4f46f70502b86e
Boris Faure <boris@fau.re>
2023-03-26 20:50:01 +0200

    qmk: handle functions with recursion

--- src/qmk.rs
+++ src/qmk.rs
@@ -19,6 +19,76 @@ pub enum QmkAction {
     DefaultLayer(u8),
     /// OSL()
     OneShotLayer(u8),
+    /// ANY()
+    Any(Box<QmkAction>),
+    /// LSFT()
+    LeftShift(Box<QmkAction>),
+    /// LCTL()
+    LeftControl(Box<QmkAction>),
+    /// LALT()
+    LeftAlt(Box<QmkAction>),
+    /// LGUI()
+    LeftGui(Box<QmkAction>),
+    /// RSFT()
+    RightShift(Box<QmkAction>),
+    /// RCTL()
+    RightControl(Box<QmkAction>),
+    /// RALT()
+    RightAlt(Box<QmkAction>),
+    /// RGUI()
+    RightGui(Box<QmkAction>),
+    /// LCA()
+    LeftControlAlt(Box<QmkAction>),
+    /// LSA()
+    LeftShiftAlt(Box<QmkAction>),
+    /// SGUI()
+    LeftShiftGui(Box<QmkAction>),
+    /// LAG()
+    LeftAltGui(Box<QmkAction>),
+    /// RCA()
+    RightControlAlt(Box<QmkAction>),
+    /// RSA()
+    RightShiftAlt(Box<QmkAction>),
+    /// RSG()
+    RightShiftGui(Box<QmkAction>),
+    /// RAG()
+    RightAltGui(Box<QmkAction>),
+    /// LCAG()
+    LeftControlAltGui(Box<QmkAction>),
+    /// MEH()
+    Meh(Box<QmkAction>),
+    /// HYPER()
+    Hyper(Box<QmkAction>),
+    /// LSFT_T()
+    LeftShiftWhenHeldOr(Box<QmkAction>),
+    /// LCTL_T()
+    LeftControlWhenHeldOr(Box<QmkAction>),
+    /// LALT_T()
+    LeftAltWhenHeldOr(Box<QmkAction>),
+    /// LGUI_T()
+    LeftGuiWhenHeldOr(Box<QmkAction>),
+    /// RSFT_T()
+    RightShiftWhenHeldOr(Box<QmkAction>),
+    /// RCTL_T()
+    RightControlWhenHeldOr(Box<QmkAction>),
+    /// RALT_T()
+    RightAltWhenHeldOr(Box<QmkAction>),
+    /// RGUI_T()
+    RightGuiWhenHeldOr(Box<QmkAction>),
+    /// C_S_T()
+    LeftControlShiftWhenHeldOr(Box<QmkAction>),
+    /// LCA_T()
+    LeftControlAltWhenHeldOr(Box<QmkAction>),
+    /// SGUI_T()
+    LeftShiftGuiWhenHeldOr(Box<QmkAction>),
+    /// LCAG_T()
+    LeftControlAltGuiWhenHeldOr(Box<QmkAction>),
+    /// RCAG_T()
+    RightControlAltGuiWhenHeldOr(Box<QmkAction>),
+    /// MEH_T()
+    LeftControlShiftAltWhenHeldOr(Box<QmkAction>),
+    /// ALL_T()
+    LeftControlShiftAltGuiWhenHeldOr(Box<QmkAction>),
 }
 
 /// Lexer item
@@ -95,7 +165,24 @@ fn parse_func_u8(func: &str, tokens: &[LexItem]) -> Result<u8, String> {
         (LexItem::Parenthesis('('), LexItem::Number(num), LexItem::Parenthesis(')')) => {
             Ok(*num as u8)
         }
-        _ => Err(String::from("func: invalid tokens")),
+        _ => Err(format!("{}: invalid tokens", func)),
+    }
+}
+
+/// Parse a function that only accepts a key paramter (usually a keycode)
+fn parse_func_kc(func: &str, tokens: &[LexItem]) -> Result<Box<QmkAction>, String> {
+    let len = tokens.len();
+    if len < 3 {
+        return Err(format!(
+            "{}: must have at least 3 tokens, got {}",
+            func, len
+        ));
+    }
+    match (&tokens[0], &tokens[len - 1]) {
+        (LexItem::Parenthesis('('), LexItem::Parenthesis(')')) => {
+            Ok(Box::new(parse(&tokens[1..len - 1])?))
+        }
+        _ => Err(format!("{}: invalid tokens", func)),
     }
 }
 
@@ -130,6 +217,93 @@ fn parse(tokens: &[LexItem]) -> Result<QmkAction, String> {
             "TT" => Ok(QmkAction::TapToggleLayer(parse_func_u8(tok, &tokens[1..])?)),
             "DF" => Ok(QmkAction::DefaultLayer(parse_func_u8(tok, &tokens[1..])?)),
             "OSL" => Ok(QmkAction::OneShotLayer(parse_func_u8(tok, &tokens[1..])?)),
+            "ANY" => Ok(QmkAction::Any(parse_func_kc(tok, &tokens[1..])?)),
+            "LSFT" => Ok(QmkAction::LeftShift(parse_func_kc(tok, &tokens[1..])?)),
+            "LCTL" => Ok(QmkAction::LeftControl(parse_func_kc(tok, &tokens[1..])?)),
+            "LALT" => Ok(QmkAction::LeftAlt(parse_func_kc(tok, &tokens[1..])?)),
+            "LGUI" => Ok(QmkAction::LeftGui(parse_func_kc(tok, &tokens[1..])?)),
+            "RSFT" => Ok(QmkAction::RightShift(parse_func_kc(tok, &tokens[1..])?)),
+            "RCTL" => Ok(QmkAction::RightControl(parse_func_kc(tok, &tokens[1..])?)),
+            "RALT" => Ok(QmkAction::RightAlt(parse_func_kc(tok, &tokens[1..])?)),
+            "RGUI" => Ok(QmkAction::RightGui(parse_func_kc(tok, &tokens[1..])?)),
+            "LCA" => Ok(QmkAction::LeftControlAlt(parse_func_kc(tok, &tokens[1..])?)),
+            "LSA" => Ok(QmkAction::LeftShiftAlt(parse_func_kc(tok, &tokens[1..])?)),
+            "SGUI" => Ok(QmkAction::LeftShiftGui(parse_func_kc(tok, &tokens[1..])?)),
+            "LAG" => Ok(QmkAction::LeftAltGui(parse_func_kc(tok, &tokens[1..])?)),
+            "RCA" => Ok(QmkAction::RightControlAlt(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "RSA" => Ok(QmkAction::RightShiftAlt(parse_func_kc(tok, &tokens[1..])?)),
+            "RSG" => Ok(QmkAction::RightShiftGui(parse_func_kc(tok, &tokens[1..])?)),
+            "RAG" => Ok(QmkAction::RightAltGui(parse_func_kc(tok, &tokens[1..])?)),
+            "LCAG" => Ok(QmkAction::LeftControlAltGui(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "MEH" => Ok(QmkAction::Meh(parse_func_kc(tok, &tokens[1..])?)),
+            "HYPER" => Ok(QmkAction::Hyper(parse_func_kc(tok, &tokens[1..])?)),
+            "LSFT_T" => Ok(QmkAction::LeftShiftWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "LCTL_T" => Ok(QmkAction::LeftControlWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "LALT_T" => Ok(QmkAction::LeftAltWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "LGUI_T" => Ok(QmkAction::LeftGuiWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "RSFT_T" => Ok(QmkAction::RightShiftWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "RCTL_T" => Ok(QmkAction::RightControlWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "RALT_T" => Ok(QmkAction::RightAltWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "RGUI_T" => Ok(QmkAction::RightGuiWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "C_S_T" => Ok(QmkAction::LeftControlShiftWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "LCA_T" => Ok(QmkAction::LeftControlAltWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "SGUI_T" => Ok(QmkAction::LeftShiftGuiWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "LCAG_T" => Ok(QmkAction::LeftControlAltGuiWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "RCAG_T" => Ok(QmkAction::RightControlAltGuiWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "MEH_T" => Ok(QmkAction::LeftControlShiftAltWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+            "ALL_T" => Ok(QmkAction::LeftControlShiftAltGuiWhenHeldOr(parse_func_kc(
+                tok,
+                &tokens[1..],
+            )?)),
+
             _ => Err(format!("unsupported command {}", tok)),
         }
     } else {
@@ -228,6 +402,24 @@ mod qmk_tests {
   ],
   "author": ""
 }"#;
+        let qmk_res = QmkKeymap::from_json_str(&json);
+        println!("{:?}", qmk_res);
+        assert!(qmk_res.is_ok());
+    }
+
+    #[test]
+    fn test_kc_recursion() {
+        let json = r#"{
+  "version": 1, "notes": "", "documentation": "", "keyboard": "", "keymap": "",
+  "layout": "", "author": "",
+  "layers": [ [
+      "LSFT_T(KC_A)",
+      "LCTL_T(KC_B)",
+      "LCA(KC_LSFT)",
+      "ANY(LCTL(LALT(KC_DEL)))",
+      "RCTL(KC_LALT)",
+      "ANY(KC_ENTER)"
+  ]]}"#;
         let qmk_res = QmkKeymap::from_json_str(&json);
         println!("{:?}", qmk_res);
         assert!(qmk_res.is_ok());
